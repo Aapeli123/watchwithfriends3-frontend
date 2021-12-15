0b1da71c48b2341591f5f6b2e0c495bbb307c942
Aapeli123 <aaposharju@gmail.com>
2021-12-15 20:22:56 +0200

    typo fixattu
    vitu typo

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -5,7 +5,7 @@ const ErrorPage = () => {
         <h1>
             Server disconnected!
         </h1>
-        <p>My server decided not to be very uwu owo and did a big fucky wucky and disconnected. I have like zero idea why this happens, but if does, I should probably restart the server. </p>
+        <p>My server decided not to be very uwu owo and did a big fucky wucky and disconnected. I have like zero idea why this happens, but if it does, I should probably restart the server. </p>
         <a href={window.location.toString()}>Try again</a>
         </>
     )
